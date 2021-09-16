6537eeeb429fe827c1a5e6b47024e72415765768
Brian Reece <bdreece@mtu.edu>
2021-09-16 12:23:46 -0400

    Refreshed Doxygen pages

--- docs/search/all_6.js
+++ docs/search/all_6.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['icm7212_0',['icm7212',['../namespaceicm7212.html',1,'']]],
-  ['icm7212_1',['ICM7212',['../classicm7212_1_1ICM7212.html#a71ac66469cc85abf371c1385fe9a745d',1,'icm7212::ICM7212::ICM7212()'],['../classicm7212_1_1ICM7212.html',1,'icm7212::ICM7212'],['../md_README.html',1,'(Global Namespace)']]],
+  ['icm7212_1',['ICM7212',['../classicm7212_1_1ICM7212.html#a71ac66469cc85abf371c1385fe9a745d',1,'icm7212::ICM7212::ICM7212()'],['../classicm7212_1_1ICM7212.html',1,'icm7212::ICM7212'],['../index.html',1,'(Global Namespace)']]],
   ['icm7212a_2',['ICM7212A',['../classicm7212_1_1ICM7212A.html',1,'icm7212::ICM7212A'],['../classicm7212_1_1ICM7212A.html#add519119c43bf1ec1664aa0ec3eecb35',1,'icm7212::ICM7212A::ICM7212A()']]],
   ['icm7212acodeb_3',['ICM7212ACodeB',['../namespaceicm7212.html#a972a4ff1a7fc22dedb23ac69af63cf65',1,'icm7212']]],
   ['icm7212am_4',['ICM7212AM',['../classicm7212_1_1ICM7212AM.html',1,'icm7212::ICM7212AM'],['../classicm7212_1_1ICM7212AM.html#a2a84e4fc0046c2ac6afce08790d6234d',1,'icm7212::ICM7212AM::ICM7212AM()']]],
