5c6d92b2f51a60beffb14255b0c53800d379d6ea
Brian Reece <bdreece@mtu.edu>
2021-10-11 00:49:20 -0400

    Added CMake AVR toolchain, updated build scripts and demo application

--- demo/main.cpp
+++ demo/main.cpp
@@ -5,17 +5,35 @@
  * @date 09/15/2021
  */
 
-#ifndef ICM7212AM_H
-#include "ICM7212AM.hpp"
-#endif
+#define F_CPU 1000000UL
+
+#include <stdint.h>
+#include <stdio.h>
 
 #include <avr/io.h>
+#include <util/delay.h>
+
+#include "ICM7212AM.hpp"
 
 int main()
 {
-  icm7212::ICM7212AM display(&PORTB, icm7212::DATA_HIGH);
+  icm7212::ICM7212AM display(&PORTB, icm7212::DIGIT_HIGH);
 
-  display.write("0000");
+  uint32_t val = 0;
+  char buf[5];
+
+  while (1)
+  {
+    if (val % 9999 == 0)
+      val = 0;
+
+    snprintf(buf, 4, "%4ld", val);
+    display.write(buf);
+
+    val++;
+
+    _delay_ms(500);
+  }
 
   return 0;
 }
